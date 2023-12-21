ef8ad8b0fe050d34c219aa81148dd6ff0ee8c347
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-21 23:54:42 +0300

    Update pulls.sh

--- pulls.sh
+++ pulls.sh
@@ -1,82 +1,84 @@
 #!/bin/bash
 
-# Проверяем, что передан один аргумент
+# Check the number of arguments
 if [ "$#" -ne 1 ]; then
-  echo "Использование: $0 <ник_пользователя_на_GitHub>"
-  exit 1
+    echo "Usage: $0 <dataset_file>"
+    exit 1
 fi
 
-# Получаем аргумент (ник пользователя)
-user=$1
+# Dataset file path
+dataset_file=$1
 
-# Директория для кэширования ответов API
-cache_dir="/home/Ega2901/github_api_cache"
-mkdir -p "$cache_dir"
+# Temporary files for gnuplot and awk
+tmp_data_file=$(mktemp /tmp/cleanliness_regression_data.XXXXXX)
+output_file="/tmp/regression_plot.png"
 
-# Функция для получения информации о пулл-реквестах из API GitHub
-function get_pulls_info() {
-  local page=$1
-  local cache_file="$cache_dir/pulls_info_page${page}.json"
+# Task 1: Average rating (overall_ratingsource)
+rating_avg=$(awk -F',' '{ if ($17 != -1) { sum += $17; count++ } } END { if (count > 0) print "RATING_AVG " sum/count }' $dataset_file)
+echo $rating_avg
 
-  # Если ответ уже закэширован, используем его
-  if [ -f "$cache_file" ]; then
-    cat "$cache_file"
-  else
-    # Запрашиваем информацию о пулл-реквестах
-    curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&creator=$user&page=$page" > "$cache_file"
-    cat "$cache_file"
-  fi
+# Task 2: Number of hotels in each country
+hotels_by_country=$(awk -F',' '{ if ($1 != -1) { country=tolower($7); count[country]++ } } END { for (c in count) print "HOTELNUMBER " c " " count[c] }' $dataset_file)
+echo "$hotels_by_country"
+
+# Task 3: Average cleanliness score per country for Holiday Inn vs. Hilton
+cleanliness_avg=$(awk -F',' '{ if ($1 != -1 && $12 != -1 && $12 != 0) { country=tolower($7); cleanliness=$12; hotel_network=tolower($2); sum[country, hotel_network] += cleanliness; count[country, hotel_network]++ } } END { for (c in count) { split(c, arr, SUBSEP); country = arr[1]; network = arr[2]; avg = sum[c] / count[c]; if (count[country, "holiday inn"] > 0 && count[country, "hilton"] > 0) print "CLEANLINESS " country " " avg " " count[country, "holiday inn"] " " count[country, "hilton"]; } }' $dataset_file)
+echo "$cleanliness_avg"
+
+# Task 4: Use gnuplot to calculate linear regression coefficients for cleanliness vs overall_ratingsource
+awk -F',' '{ if ($12 != -1 && $17 != -1) print $12, $17 }' $dataset_file > $tmp_data_file
+
+# Task 5: Use awk to calculate average cleanliness and overall_ratingsource
+awk -F',' '
+BEGIN {
+    clean_sum = 0;
+    overall_sum = 0;
+    clean_count = 0;
+    overall_count = 0;
 }
 
-# Инициализируем переменные
-total_pulls_count=0
-earliest_pull_number=""
-earliest_pull_merged=""
+{
+    if ($12 != -1 && $12 != 0) {
+        clean_sum += $12;
+        clean_count++;
+    }
 
-# Обрабатываем пулл-реквесты по страницам
-page=1
-while true; do
-  pulls_info=$(get_pulls_info "$page")
+    if ($17 != -1) {
+        overall_sum += $17;
+        overall_count++;
+    }
+}
 
-  # Проверяем, что пользователь существует
-  if [[ $(echo "$pulls_info" | jq '.message') == "Not Found" ]]; then
-    echo "Пользователь $user не найден на GitHub."
-    exit 1
-  fi
+END {
+    if (clean_count > 0) {
+        clean_avg = clean_sum / clean_count;
+        print "CLEAN_AVG " clean_avg;
+    }
 
-  # Получаем количество пулл-реквестов на текущей странице
-  page_pulls_count=$(echo "$pulls_info" | jq length)
+    if (overall_count > 0) {
+        overall_avg = overall_sum / overall_count;
+        print "OVERALL_AVG " overall_avg;
+    }
+}
+' $dataset_file
 
-  # Прерываем цикл, если больше нет пулл-реквестов
-  if [ "$page_pulls_count" -eq 0 ]; then
-    break
-  fi
+# Task 6: Use gnuplot to create a linear regression plot
+gnuplot <<EOF
+set terminal pngcairo enhanced font 'Verdana,12' size 800,600
+set output "$output_file"
 
-  # Обновляем общее количество пулл-реквестов
-  total_pulls_count=$((total_pulls_count + page_pulls_count))
+set title "Linear Regression: Cleanliness vs Overall Rating"
+set xlabel "Overall Rating"
+set ylabel "Cleanliness"
 
-  # Поиск самого раннего пулл-реквеста
-  earliest_pull=$(echo "$pulls_info" | jq -r '.[0] | {number: .number, merged: .merged}')
+f(x) = a * x + b
+fit f(x) "$tmp_data_file" using 1:2 via a, b
 
-  # Обновляем информацию о самом раннем пулл-реквесте
-  if [ -z "$earliest_pull_number" ] || [ "${earliest_pull.number}" -lt "$earliest_pull_number" ]; then
-    earliest_pull_number="${earliest_pull.number}"
-    earliest_pull_merged="${earliest_pull.merged}"
-  fi
+plot "$tmp_data_file" using 1:2 with points title "Data Points", \
+     f(x) with lines title sprintf("Regression Line: y = %.2fx + %.2f", a, b)
+EOF
 
-  # Увеличиваем счетчик страниц
-  page=$((page + 1))
-done
+echo "Regression plot saved to: $output_file"
 
-# Выводим количество пулл-реквестов
-echo "PULLS $total_pulls_count"
-
-# Выводим номер самого раннего пулл-реквеста
-echo "EARLIEST $earliest_pull_number"
-
-# Выводим бинарный флаг MERGED
-if [ "$earliest_pull_merged" == "true" ]; then
-  echo "MERGED 1"
-else
-  echo "MERGED 0"
-fi
+# Cleanup temporary files
+rm -f $tmp_data_file
