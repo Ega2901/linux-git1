af7a494a7d872de1c3745eed35df9fc7e214ee96
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-21 23:40:35 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,34 +1,34 @@
-#!/usr/bin/awk -f
+#!/bin/bash
 
-# Проверка наличия аргумента
+# Check the number of arguments
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <dataset_file>"
     exit 1
 fi
 
-# Путь к файлу с датасетом
+# Dataset file path
 dataset_file=$1
 
-# Временные файлы для gnuplot и awk
+# Temporary files for gnuplot and awk
 tmp_data_file=$(mktemp /tmp/cleanliness_regression_data.XXXXXX)
 output_file="/tmp/regression_plot.png"
 
-# Задача 1: Средний рейтинг (overall_ratingsource)
+# Task 1: Average rating (overall_ratingsource)
 rating_avg=$(awk -F',' '{ if ($17 != -1) { sum += $17; count++ } } END { if (count > 0) print "RATING_AVG " sum/count }' $dataset_file)
 echo $rating_avg
 
-# Задача 2: Число отелей в каждой стране
+# Task 2: Number of hotels in each country
 hotels_by_country=$(awk -F',' '{ if ($1 != -1) { country=tolower($7); count[country]++ } } END { for (c in count) print "HOTELNUMBER " c " " count[c] }' $dataset_file)
 echo "$hotels_by_country"
 
-# Задача 3: Средний балл cleanliness по стране для отелей Holiday Inn vs. Hilton
+# Task 3: Average cleanliness score per country for Holiday Inn vs. Hilton
 cleanliness_avg=$(awk -F',' '{ if ($1 != -1 && $12 != -1) { country=tolower($7); cleanliness=$12; hotel_network=tolower($2); sum[country, hotel_network] += cleanliness; count[country, hotel_network]++ } } END { for (c in count) { split(c, arr, SUBSEP); country = arr[1]; network = arr[2]; avg = sum[c] / count[c]; if (count[country, "holiday inn"] > 0 && count[country, "hilton"] > 0) print "CLEANLINESS " country " " avg " " count[country, "holiday inn"] " " count[country, "hilton"]; } }' $dataset_file)
 echo "$cleanliness_avg"
 
-# Задача 4: Используя gnuplot рассчитайте коэффициенты линейной регрессии для cleanliness от overall_ratingsource
+# Task 4: Use gnuplot to calculate linear regression coefficients for cleanliness vs overall_ratingsource
 awk -F',' '{ if ($12 != -1 && $17 != -1) print $12, $17 }' $dataset_file > $tmp_data_file
 
-# Задача 5: Используя awk рассчитайте среднее значение cleanliness и overall_ratingsource
+# Task 5: Use awk to calculate average cleanliness and overall_ratingsource
 awk -F',' '
 BEGIN {
     clean_sum = 0;
@@ -62,6 +62,7 @@ END {
 }
 ' $dataset_file
 
+# Task 6: Use gnuplot to create a linear regression plot
 gnuplot <<EOF
 set terminal pngcairo enhanced font 'Verdana,12' size 800,600
 set output "$output_file"
@@ -79,5 +80,5 @@ EOF
 
 echo "Regression plot saved to: $output_file"
 
-# Удаление временных файлов
+# Cleanup temporary files
 rm -f $tmp_data_file
