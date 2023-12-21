ee7e365e0db1e320cb6be7c3cad836cd31104d77
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-21 23:07:58 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -12,8 +12,9 @@ awk -F ',' 'NR>1 && $18!=-1 {sum+=$18; count++} END {if (count>0) print "RATING_
 # Шаг 4: Вычленение страны из поля doc_id и подсчет количества отелей в каждой стране
 awk -F ',' 'NR>1 {split($1, parts, "_"); country=tolower(parts[1]); hotel_count[country]++} END {for (c in hotel_count) print "HOTELNUMBER", c, hotel_count[c]}' "$1"
 
-# Шаг 5: Вычисление среднего балла cleanliness по стране для отелей Holiday Inn и Hilton
+# Шаг 5: Вычисление среднего балла cleanliness по стране для отелей Holiday Inn vs. отелей Hilton
 awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[2]); if (country == "holiday" || country == "hilton") {sum[country]+=$13; count[country]++}} END {for (c in sum) if (count[c]>0) print "CLEANLINESS", c, sum[c]/count[c]}' "$1"
+
 
 # Шаг 6: Вывод значений cleanliness для каждой страны
 awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[1]); print "CLEANLINESS", country, $13}' "$1"
