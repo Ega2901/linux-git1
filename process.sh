5863691100223c24509176a24cdd8fbd3e3ed2b9
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-21 23:21:07 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -13,7 +13,7 @@ awk -F ',' 'NR>1 && $16!=-1 {sum+=$16; count++} END {if (count>0) print "RATING_
 awk -F ',' 'NR>1 {split($7, parts, "_"); country=tolower(parts[1]); count[country]++} END {for (c in count) if (count[c]>0) print "HOTELNUMBER", c, count[c]}' "$1"
 
 # Step 3: Calculate the average cleanliness score per country for Holiday Inn and Hilton
-awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[2]); if (country == "holiday" || country == "hilton") {sum[country]+=$13; count[country]++}} END {for (c in sum) if (count[c]>0) print "CLEANLINESS", c, sum[c]/count[c]}' "$1"
+awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[2]); if (country == "holiday" || country == "hilton") {sum[country]+=$13; count[country]++}} END {for (c in sum) if (count[c]>0) print "CLEANLINESS", c, "holidayinn", "hilton", sum[c]/count[c]}' "$1"
 
 # Step 4: Generate data for gnuplot and calculate linear regression coefficients
 awk -F ',' 'NR>1 && $13!=-1 && $16!=-1 {print $13, $16}' "$1" > /tmp/temp_data.txt
