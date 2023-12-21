71b3cdc0242df764dd98edb93f62696df67cdbbc
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-22 00:18:02 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,84 +1,44 @@
 #!/bin/bash
 
-# Check the number of arguments
+# Проверяем, передан ли файл с датасетом в аргументах
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <dataset_file>"
+    echo "Использование: $0 <путь_к_файлу_датасета>"
     exit 1
 fi
 
-# Dataset file path
-dataset_file=$1
+# Путь к файлу датасета
+dataset="$1"
 
-# Temporary files for gnuplot and awk
-tmp_data_file=$(mktemp /tmp/cleanliness_regression_data.XXXXXX)
-output_file="/tmp/regression_plot.png"
+# Создаем временный файл для gnuplot в /tmp
+gnuplot_data=$(mktemp /tmp/gnuplot_data.XXXXXX)
 
-# Task 1: Average rating (overall_ratingsource)
-rating_avg=$(awk -F',' '{ if ($17 != -1) { sum += $17; count++ } } END { if (count > 0) print "RATING_AVG " sum/count }' $dataset_file)
-echo "Task 1: $rating_avg"
+# Извлекаем данные для линейной регрессии, исключая записи с overall_ratingsource = -1
+awk -F, 'NR > 1 && $17 != -1 { printf "%s %s\n", $17, $12 }' "$dataset" > "$gnuplot_data"
 
-# Task 2: Number of hotels in each country
-hotels_by_country=$(awk -F',' '{ if ($1 != -1) { country=tolower($7); count[country]++ } } END { for (c in count) print "HOTELNUMBER " c " " count[c] }' $dataset_file)
-echo "Task 2: $hotels_by_country"
+# Вычисляем коэффициенты линейной регрессии
+regression_coefficients=$(gnuplot -e "f(x) = a*x + b; fit f(x) '$gnuplot_data' via a, b; print a, b")
 
-# Task 3: Average cleanliness score per country for Holiday Inn vs. Hilton
-cleanliness_avg=$(awk -F',' '{ if ($1 != -1 && $12 != -1 && $12 != 0) { country=tolower($7); cleanliness=$12; hotel_network=tolower($2); sum[country, hotel_network] += cleanliness; count[country, hotel_network]++ } } END { for (c in count) { split(c, arr, SUBSEP); country = arr[1]; network = arr[2]; avg = sum[c] / count[c]; if (count[country, "holiday inn"] > 0 && count[country, "hilton"] > 0) print "CLEANLINESS " country " " avg " " count[country, "holiday inn"] " " count[country, "hilton"]; } }' $dataset_file)
-echo "Task 3: $cleanliness_avg"
+# Извлекаем коэффициенты из вывода
+slope=$(echo "$regression_coefficients" | awk '{print $1}')
+intercept=$(echo "$regression_coefficients" | awk '{print $2}')
 
-# Task 4: Use gnuplot to calculate linear regression coefficients for cleanliness vs overall_ratingsource
-awk -F',' '{ if ($12 != -1 && $17 != -1) print $12, $17 }' $dataset_file > $tmp_data_file
+# Генерируем скрипт для gnuplot
+gnuplot_script=$(mktemp /tmp/gnuplot_script.XXXXXX)
+echo "set xlabel 'Overall Rating'" > "$gnuplot_script"
+echo "set ylabel 'Cleanliness'" >> "$gnuplot_script"
+echo "set title 'Linear Regression: Cleanliness vs Overall Rating'" >> "$gnuplot_script"
+echo "plot '$gnuplot_data' title 'Data', $slope*x + $intercept title 'Linear Regression' with lines" >> "$gnuplot_script"
 
-# Task 5: Use awk to calculate average cleanliness and overall_ratingsource
-awk -F',' '
-BEGIN {
-    clean_sum = 0;
-    overall_sum = 0;
-    clean_count = 0;
-    overall_count = 0;
-}
+# Имя файла для сохранения графика в /tmp
+output_file=$(mktemp /tmp/linear_regression_plot.XXXXXX.png)
 
-{
-    if ($12 != -1 && $12 != 0) {
-        clean_sum += $12;
-        clean_count++;
-    }
+# Запускаем gnuplot
+gnuplot -p "$gnuplot_script"
 
-    if ($17 != -1) {
-        overall_sum += $17;
-        overall_count++;
-    }
-}
+# Удаляем временные файлы
+rm "$gnuplot_data" "$gnuplot_script"
 
-END {
-    if (clean_count > 0) {
-        clean_avg = clean_sum / clean_count;
-        print "CLEAN_AVG " clean_avg;
-    }
+# Перемещаем график в /tmp
+mv "linear_regression_plot.png" "$output_file"
 
-    if (overall_count > 0) {
-        overall_avg = overall_sum / overall_count;
-        print "OVERALL_AVG " overall_avg;
-    }
-}
-' $dataset_file
-
-# Task 6: Use gnuplot to create a linear regression plot
-gnuplot <<EOF
-set terminal pngcairo enhanced font 'Verdana,12' size 800,600
-set output "$output_file"
-
-set title "Linear Regression: Cleanliness vs Overall Rating"
-set xlabel "Overall Rating"
-set ylabel "Cleanliness"
-
-f(x) = a * x + b
-fit f(x) "$tmp_data_file" using 1:2 via a, b
-
-plot "$tmp_data_file" using 1:2 with points title "Data Points", \
-     f(x) with lines title sprintf("Regression Line: y = %.2fx + %.2f", a, b)
-EOF
-
-echo "Regression plot saved to: $output_file"
-
-# Cleanup temporary files
-rm -f $tmp_data_file
+echo "График сохранен в файл: $output_file"
