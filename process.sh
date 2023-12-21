ef3c84c3068ccbfecfcea05c0a9bf324235ea56c
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2023-12-21 23:16:22 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,46 +1,20 @@
-#!/bin/bash
+#!/usr/bin/awk -f
 
-# Шаг 1: Создание временного файла для данных линейной регрессии
-temp_file="/tmp/temp_data.txt"
-
-# Шаг 2: Формирование скрипта gnuplot
-gnuplot_script="/tmp/gnuplot_script.gp"
-
-# Шаг 3: Вычисление среднего рейтинга
-awk -F ',' 'NR>1 && $18!=-1 {sum+=$18; count++} END {if (count>0) print "RATING_AVG", sum/count; else print "RATING_AVG", "N/A"}' "$1"
-
-# Шаг 4: Вычленение страны из поля doc_id и подсчет количества отелей в каждой стране
-awk -F ',' 'NR>1 {split($1, parts, "_"); country=tolower(parts[1]); hotel_count[country]++} END {for (c in hotel_count) print "HOTELNUMBER", c, hotel_count[c]}' "$1"
-
-# Шаг 5: Вычисление среднего балла cleanliness по стране для отелей Holiday Inn vs. отелей Hilton
-awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[2]); if (country == "holiday" || country == "hilton") {sum[country]+=$13; count[country]++}} END {for (c in sum) if (count[c]>0) print "CLEANLINESS", c, sum[c]/count[c]}' "$1"
-
-
-# Шаг 6: Вывод значений cleanliness для каждой страны
-awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[1]); print "CLEANLINESS", country, $13}' "$1"
-
-# Шаг 7: Фильтрация данных для линейной регрессии и сохранение во временный файл
-awk -F ',' 'NR>1 && $13!=-1 && $18!=-1 {print $18, $13}' "$1" > "$temp_file"
-
-# Проверка, что временный файл содержит данные
-if [ ! -s "$temp_file" ]; then
-    echo "Error: No valid data for gnuplot."
+# Проверка передачи аргумента
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <dataset_file.csv>"
     exit 1
 fi
 
-# Шаг 8: Запись скрипта gnuplot
-cat > "$gnuplot_script" <<EOL
-set term png
-set output "/tmp/linear_regression_plot.png"
-set xlabel "Overall Rating"
-set ylabel "Cleanliness"
-set title "Линейная регрессия: Чистота vs. Общий рейтинг"
-set datafile separator ","
-f(x) = m*x + b
-fit f(x) '$temp_file' using 1:2 via m, b
-plot "$temp_file" using 1:2 title "Точки данных" with points pointtype 7 pointsize 1.5, \
-     f(x) title "Линейная регрессия"
-EOL
+# Шаг 1: Вычисление среднего рейтинга (overall_ratingsource)
+awk -F ',' 'NR>1 && $16!=-1 {sum+=$16; count++} END {if (count>0) print "RATING_AVG", sum/count}' "$1"
 
-# Запуск gnuplot с игнорированием ошибок при вычислении коэффициентов линейной регрессии
-gnuplot "$gnuplot_script" 2>/dev/null || true
+# Шаг 2: Вычисление числа отелей в каждой стране
+awk -F ',' 'NR>1 {split($7, parts, "_"); country=tolower(parts[1]); count[country]++} END {for (c in count) if (count[c]>0) print "HOTELNUMBER", c, count[c]}' "$1"
+
+# Шаг 3: Вычисление среднего балла cleanliness по стране для отелей Holiday Inn vs. отелей Hilton
+awk -F ',' 'NR>1 && $13!=-1 {split($1, parts, "_"); country=tolower(parts[2]); if (country == "holiday" || country == "hilton") {sum[country]+=$13; count[country]++}} END {for (c in sum) if (count[c]>0) print "CLEANLINESS", c, sum[c]/count[c]}' "$1"
+
+# Шаг 4: Генерация данных для gnuplot и расчет коэффициентов линейной регрессии
+awk -F ',' 'NR>1 && $13!=-1 && $16!=-1 {print $13, $16}' "$1" > /tmp/temp_data.txt
+gnuplot -e "input='/tmp/temp_data.txt'; output='/tmp/plot.png'" -c gnuplot_script.gp
